c21f83237aca8392b39ac700f018c301cb100275
willieLjohnson <liwa.johnson@gmail.com>
2020-06-24 14:21:13 -0400

    Add blink ability

--- Player.gd
+++ Player.gd
@@ -5,6 +5,7 @@ var acceleration = 20
 var gravity = 9.8
 var jump = 5
 var jump_num = 0
+var blink_dist = 7
 
 var mouse_sensitivity = 0.05
 
@@ -38,22 +39,30 @@ func _process(delta: float) -> void:
 		elif jump_num == 1 and not is_on_floor():
 			fall.y = jump
 			jump_num = 2
-			
+		
 	if Input.is_action_just_pressed("ui_cancel"):
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	
 	if Input.is_action_pressed("move_forward"):
 		direction -= transform.basis.z
+		if Input.is_action_just_pressed("ability"):
+			 translate(Vector3(0, 0, -blink_dist))
 		
 	elif Input.is_action_pressed("move_backward"):
 		direction += transform.basis.z
-	
+		if Input.is_action_just_pressed("ability"):
+			 translate(Vector3(0, 0, blink_dist))
+		
 	if Input.is_action_pressed("move_left"):
 		direction -= transform.basis.x
+		if Input.is_action_just_pressed("ability"):
+			 translate(Vector3(-blink_dist, 0, 0))
 		
 	elif Input.is_action_pressed("move_right"):
 		direction += transform.basis.x 
-
+		if Input.is_action_just_pressed("ability"):
+			 translate(Vector3(blink_dist, 0, 0))
+				
 	direction = direction.normalized()
 	velocity = velocity.linear_interpolate(direction * speed, acceleration * delta)
 	velocity = move_and_slide(velocity, Vector3.UP)
