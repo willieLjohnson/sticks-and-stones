9bc3cbbd4b72d3ced431a76148d3de1fb7f2e57f
Willie Johnson <liwa.johnson@gmail.com>
2020-06-25 11:04:40 -0400

    Add wall running

--- Player.gd
+++ Player.gd
@@ -28,10 +28,14 @@ var hook_point = Vector3()
 
 var damage = 100
 
+var wall_normal
+var can_wallrun = false
+
 onready var head = $Head
 onready var pcap = $CollisionShape
 onready var bonker = $HeadBonker
 onready var sprint_timer = $SprintTimer
+onready var wall_run_timer = $WallRunTimer
 onready var aim_cast = $Head/Camera/AimCast
 onready var grapple_cast = $Head/Camera/GrappleCast
 onready var muzzle = $Head/Gun/Muzzle
@@ -68,15 +72,56 @@ func grapple():
 		hook_point = null
 		hook_point_get = false
 		global_translate(Vector3(0, -1, 0))
-		
-func _physics_process(delta: float) -> void:
-	grapple()
-	
+
+func wall_run():
+	if can_wallrun:
+		if Input.is_action_pressed("jump"):
+			if Input.is_action_pressed("move_forward"):
+				if is_on_wall():
+					wall_normal = get_slide_collision(0)
+					yield(get_tree().create_timer(0.2), "timeout")
+					fall.y = 0
+					direction = -wall_normal.normal * speed
+				
+func _physics_process(delta: float) -> void:	
 	var head_bonked = false
 	speed = default_move_speed
 	
 	direction = Vector3()
 	
+	move_and_slide(fall, Vector3.UP, true)
+	
+
+	if bonker.is_colliding():
+		head_bonked = true
+		
+	if not is_on_floor():
+		fall.y -= gravity * delta
+	else:
+		jump_num = 0
+		can_wallrun = false
+	
+	grapple()
+	wall_run()
+		
+	if Input.is_action_just_pressed("jump"):
+		if jump_num == 0 and is_on_floor():
+			fall.y = jump
+			jump_num = 1
+			can_wallrun = true
+			wall_run_timer.start()
+		elif jump_num == 1 and not is_on_floor():
+			fall.y = jump
+			jump_num = 2
+		
+	if head_bonked:
+		fall.y = -2
+		
+
+	if Input.is_action_just_pressed("ui_cancel"):
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	
+		
 	if Input.is_action_just_pressed("fire"):
 		if aim_cast.is_colliding():
 			var bullet = get_world().direct_space_state
@@ -87,30 +132,8 @@ func _physics_process(delta: float) -> void:
 				if target.is_in_group("Enemy"):
 					print("Hit enemy")
 					target.health -= damage
-				
-	if bonker.is_colliding():
-		head_bonked = true
-		
-	if not is_on_floor():
-		fall.y -= gravity * delta
-	else:
-		jump_num = 0
-		
-	if Input.is_action_just_pressed("jump"):
-		if jump_num == 0 and is_on_floor():
-			fall.y = jump
-			jump_num = 1
-		elif jump_num == 1 and not is_on_floor():
-			fall.y = jump
-			jump_num = 2
-		
-	if head_bonked:
-		fall.y = -2
-		
-	if Input.is_action_just_pressed("ui_cancel"):
-		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-		
-		
+			
+
 	if Input.is_action_pressed("crouch"):
 		pcap.shape.height -= crouch_speed * delta
 		speed = crouch_move_speed
@@ -143,8 +166,12 @@ func _physics_process(delta: float) -> void:
 	direction = direction.normalized()
 	velocity = velocity.linear_interpolate(direction * speed, acceleration * delta)
 	velocity = move_and_slide(velocity, Vector3.UP)
-	move_and_slide(fall, Vector3.UP, true)
+
 
 
 func _on_SprintTimer_timeout() -> void:
 	sprinting = false
+
+
+func _on_WallRunTimer_timeout() -> void:
+	can_wallrun = false
