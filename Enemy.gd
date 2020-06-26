5a094c30a12051a0017d17b7d4c15cb719a8d1c1
Willie Johnson <liwa.johnson@gmail.com>
2020-06-26 09:25:34 -0400

    Add area to enemy for detecting player and rotating to location

--- Enemy.gd
+++ Enemy.gd
@@ -10,25 +10,34 @@ enum  {
 
 var state = IDLE
 
+var target
+
+const TURN_SPEED = 2
+
 onready var raycast = $RayCast
 onready var animation_player = $dummymale/AnimationPlayer
+onready var eyes = $Eyes
 
 func _ready() -> void:
 	pass
 
-func _process(delta: float) -> void:
-	if raycast.is_colliding() and raycast.get_collider().is_in_group("Player"):
+func _on_SightRange_body_entered(body: Node) -> void:
+	if body.is_in_group("Player"):
 		state = ALERT
-	elif Input.is_action_pressed("fire"):
-		state = STUNNED
-	else:
-		state = IDLE
-		
+		target = body
+
+
+func _on_SightRange_body_exited(body: Node) -> void:
+	state = IDLE
+
+func _process(delta: float) -> void:
 	match state:
 		IDLE:
 			animation_player.play("Idle")
 		ALERT:
 			animation_player.play("Alert")
+			eyes.look_at(target.global_transform.origin, Vector3.UP)
+			rotate_y(deg2rad(eyes.rotation.y * TURN_SPEED))
 		STUNNED:
 			animation_player.play("Stunned")
 			
